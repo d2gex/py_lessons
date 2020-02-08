98aa3bef68b31487b05c67c8539ae1b311a5e377
d2gex <daniel.garcia@d2garcia.com>
2020-02-08 15:16:42 +0000

    tuples and unpacking

--- master.py
+++ master.py
@@ -1,9 +1,21 @@
-import library_1
-import library_2
-from package_1 import library_3
+def print_numbers(x, y, z):
+    print(x, y, z)
+
 
 if __name__ == '__main__':
-    library_1.print_name()
-    library_2.print_name()
-    library_3.print_name()
-
+    _tuple = (1, 2, 3)
+    # _tuple[1] = 6 --> This is not possible because it is inmutable
+    tuple_1 = 1, 2, 3
+    print(type(tuple_1))
+    a = tuple_1[0]
+    b = tuple_1[1]
+    c = tuple_1[2]
+    print(a, b, c)
+    x, z, y = tuple_1
+    print(x, z, y)
+    x, *rest = tuple_1
+    print(x)
+    print(rest)
+    print(*rest)
+    print_numbers(*tuple_1)
+    print(type(tuple_1))
