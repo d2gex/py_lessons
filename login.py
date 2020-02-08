3d926666e93472e0eafada1f3b28b587c266b4c2
d2gex <daniel.garcia@d2garcia.com>
2020-02-08 19:39:15 +0000

    Read credentials from a file

--- login.py
+++ login.py
@@ -1,5 +1,5 @@
 #  Return soon, Return often
-def login_2(username, password, credentials):
+def check_login(username, password, credentials):
 
     if username == credentials['login'] and password == credentials['password']:
         return 'it is correct'
@@ -11,3 +11,16 @@ def login_2(username, password, credentials):
         return 'only password correct'
 
     return 'none correct'
+
+
+def get_credentials(filepath):
+    lines = []
+    with open(filepath, 'r') as fh:
+        for line in fh:
+            lines.append(line.strip('\n'))
+    login, login_value = lines[0].split(':')
+    password, password_value = lines[-1].split(':')
+    return {
+        login: login_value,
+        password: password_value
+    }
--- master.py
+++ master.py
@@ -1,13 +1,15 @@
 import login
 
-credentials = {
-    'login': 'abc',
-    'password': '123'
-}
-
 if __name__ == '__main__':
+    #  we read the credentials from text file
+    credentials = login.get_credentials('.credentials')
+
+    #  we get the user's login and password from the keyboard
     login_entered = input('enter the login ID ')
     password_entered = input('enter the password ')
-    result = login.login_2(login_entered, password_entered, credentials)
+
+    #  we run our business logic to ensure the credentials are the correct one
+    result = login.check_login(login_entered, password_entered, credentials)
+    # print(credentials)
     print(result)
 
