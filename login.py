bf0f062384e524f70100a01d5b94a88928301bb9
d2gex <daniel.garcia@d2garcia.com>
2020-02-08 19:12:13 +0000

    Add credentias as a Python dictionary

--- login.py
+++ login.py
@@ -1,18 +1,13 @@
-
-LOGIN = 'abc'
-PASSWORD = '123'
-
-
 #  Return soon, Return often
-def login_2(username, password):
+def login_2(username, password, credentials):
 
-    if username == LOGIN and password == PASSWORD:
+    if username == credentials['login'] and password == credentials['password']:
         return 'it is correct'
 
-    if username != LOGIN and password == PASSWORD:
+    if username != credentials['login'] and password == credentials['password']:
         return 'only login ID correct'
 
-    if username == LOGIN and password != PASSWORD:
+    if username == credentials['login'] and password != credentials['password']:
         return 'only password correct'
 
     return 'none correct'
--- master.py
+++ master.py
@@ -1,9 +1,13 @@
 import login
 
+credentials = {
+    'login': 'abc',
+    'password': '123'
+}
 
 if __name__ == '__main__':
     login_entered = input('enter the login ID ')
     password_entered = input('enter the password ')
-    result = login.login_2(login_entered, password_entered)
+    result = login.login_2(login_entered, password_entered, credentials)
     print(result)
 
